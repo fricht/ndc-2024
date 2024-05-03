2e61d7f7727ad141da6966b83174f1b92f7f27ae
Ptitet <ptitet73@gmail.com>
2024-05-03 13:20:38 +0200

    test de base

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pyxel
 
+class Game:
+    def __init__(self, width, height, title):
+        pyxel.init(width, height, title=title)
+        self.x = 0
+        pyxel.run(self.update, self.render)
+    
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
+    
+    def render(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9)
 
-
-class MainGame:
-    screen = None
-
-
-
-
-if __name__ == '__main__':
-    pyxel.init(128, 128, 'Window Name')
-    pyxel.update()
+Game(128, 128, 'lol ca marche')
