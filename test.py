ae8b955b2018705546ab1158389ec88c4a97e7e9
Ptitet <ptitet73@gmail.com>
2024-05-03 16:21:29 +0200

    chemin

--- test.py
+++ test.py
@@ -10,5 +10,5 @@ images = pyxel.images[tilemap.imgsrc]
 slime_coord = tilemap.pget(129, 127)
 slime = images.pget(slime_coord[0], slime_coord[1])
 
-pyxel.bltm(0, 0, tilemap, 120, 120, 256, 256)
+pyxel.bltm(0, 0, tilemap, 0, 0, 256, 256)
 pyxel.show()
