4cc52385d8888a3c3b3604d9284d7e475818bd37
Gedeon.D <gedeon@deveaux.fr>
2024-05-03 13:15:37 +0200

    init

--- main.py
+++ main.py
@@ -3,11 +3,33 @@ import pyxel
 
 
 class MainGame:
-    screen = None
+    def __init__(self):
+        self.screen = MainMenu()
+
+    def update(self):
+        self.screen.update()
+
+    def render(self):
+        pyxel.cls(0)
+        self.screen.render()
+
+
+
+class Screen: # abstract class
+    def update(self):
+        pass
+    def render(self):
+        pass
+
+
+class MainMenu(Screen):
+    def render(self):
+        pyxel.rect(10, 10, 15, 15, 1)
 
 
 
 
 if __name__ == '__main__':
+    game = MainGame()
     pyxel.init(128, 128, 'Window Name')
-    pyxel.update()
+    pyxel.run(game.update, game.render)
